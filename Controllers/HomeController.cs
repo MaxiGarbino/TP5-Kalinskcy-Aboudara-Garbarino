e1f573d4f5336150c8087f42dba4de7097a07dd9
MAXIMILIANO GARBARINO <47433817@ort>
2023-05-30 09:00:46 -0300

    iniciando tp

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,9 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Tutorial(){
+        return View();
+    }
 
     public IActionResult Privacy()
     {
