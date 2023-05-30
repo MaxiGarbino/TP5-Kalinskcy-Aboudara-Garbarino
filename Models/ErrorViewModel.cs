f261f27b0ea8d51157c54ff1a4d4cad852f20ac2
MAXIMILIANO GARBARINO <47433817@ort>
2023-05-30 08:55:21 -0300

    iniciando trabajo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP5_Kalinscky_Aboudara_Garbarino.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
